364b4269037aedca26e24eea1de1fd451bcf43e2
sidcodes1011 <sidcodes1011@gmail.com>
2026-04-19 14:06:44 +0530

    Corrected Folder Structure

--- Tracking Algorithm/Array Traversal/Prefix Sum/SubarraySumEqualsK.java
+++ Tracking Algorithm/Array Traversal/Prefix Sum/SubarraySumEqualsK.java
@@ -1,5 +1,5 @@
+
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 /*
@@ -10,10 +10,9 @@ Given an array of integers nums and an integer k, return the total number of sub
 A subarray is a contiguous non-empty sequence of elements within an array.
 
  */
-
 public class SubarraySumEqualsK {
 
-	// Normal approch  Time complexity  ::: O(n^2)
+    // Normal approch  Time complexity  ::: O(n^2)
 /*	public static int subarraySumCount(int[] nums, int k) {
 
 		int count = 0;
@@ -32,37 +31,35 @@ public class SubarraySumEqualsK {
 		}
 		return count;
 	}
-*/
-	
-	// Approach for time complexity O(n)
-	public static int subarraySumCount(int[] nums, int k) {
-	    Map<Integer, Integer> map = new HashMap<>();
-	    map.put(0, 1);
+     */
+    // Approach for time complexity O(n)
+    public static int subarraySumCount(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
 
-	    int prefixSum = 0;
-	    int count = 0;
+        int prefixSum = 0;
+        int count = 0;
 
-	    for (int num : nums) {
-	        prefixSum += num;
+        for (int num : nums) {
+            prefixSum += num;
 
-	        if (map.containsKey(prefixSum - k)) {
-	            count += map.get(prefixSum - k);
-	        }
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
 
-	        map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
-	    }
-	    return count;
-	}
-	
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+        return count;
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int nums[] = {0,3,3};
-		int sum = 3;
-		int result =subarraySumCount(nums,sum);
+        int nums[] = {0, 3, 3};
+        int sum = 3;
+        int result = subarraySumCount(nums, sum);
 
-		System.out.println("The count of pair resulting ::: " +sum+ " is " + result);
-	}
+        System.out.println("The count of pair resulting ::: " + sum + " is " + result);
+    }
 }
 
 /*
@@ -103,6 +100,4 @@ Works with NEGATIVE numbers (Sliding Window fails here)
 "Count subarrays" + "Sum = K" → Think PREFIX SUM + HASHMAP
 
 ==========================================================
-*/
-
-
+ */
