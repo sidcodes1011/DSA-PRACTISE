3a01265f2c5c25fbf96e9f2412e50def1bfb1e8d
sidcodes1011 <sidcodes1011@gmail.com>
2026-04-22 19:37:12 +0530

    Add files via upload

--- Tracking Algorithm/Array Traversal/Two Pointer/RemoveDuplicatesFromSortedArray.java
+++ Tracking Algorithm/Array Traversal/Two Pointer/RemoveDuplicatesFromSortedArray.java
@@ -1,3 +1,5 @@
+package localrun;
+
 /*
 Question:
 LeetCode 26 - Remove Duplicates from Sorted Array
@@ -22,19 +24,19 @@ public class RemoveDuplicatesFromSortedArray {
     
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
-    int i = 0;
-    for (int j = 1; j < nums.length; j++) {
-        if (nums[j] != nums[i]) {
-            i++;
-            nums[i] = nums[j];
+    int left = 0;
+    for (int right = 1; right < nums.length; right++) {
+        if (nums[right] != nums[left]) {
+            left++;
+            nums[left] = nums[right];
         }
     }
-    return i + 1;
+    return left + 1;
     }
 
     public static void main(String[] args) {
         
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums = {1,1,2};
 
         int result = removeDuplicates(nums);
 
